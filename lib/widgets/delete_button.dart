197f47ace8f8ad7ae102e6db55c51d3ce5e4d8bd
tetsuka <rogue1starwars728@yahoo.co.jp>
2025-02-01 11:03:57 +0900

    refactoring

---
+++ lib/widgets/delete_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:phoneduino_block/provider/block_tree_provider.dart';
+
+class DeleteButton extends ConsumerWidget {
+  final String id;
+  const DeleteButton({
+    super.key,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return IconButton(
+      onPressed: () {
+        ref.read(blockTreeProvider.notifier).deleteBlock(id: id);
+      },
+      icon: const Icon(Icons.delete),
+    );
+  }
+}
