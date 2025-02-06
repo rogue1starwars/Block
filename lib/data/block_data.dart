3940dc1a624a778990f0e999548fde192d7aa2d4
tetsuka <rogue1starwars728@yahoo.co.jp>
2025-02-06 13:07:44 +0900

    added environment sensors

--- lib/data/block_data.dart
+++ lib/data/block_data.dart
@@ -12,6 +12,7 @@ import 'package:phoneduino_block/provider/ui_provider.dart';
 import 'package:phoneduino_block/provider/variables_provider.dart';
 import 'package:phoneduino_block/utils/file_logger.dart';
 import 'package:phoneduino_block/utils/type.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class BlockBluePrint {
   final String name;
@@ -144,9 +145,9 @@ List<BlockBluePrint> blockData = [
               );
           return;
         }
-        if (value < 1000) {
+        if (value < 100) {
           ref.read(uiProvider.notifier).showMessage(
-                'Interval must be at least 1000ms',
+                'Interval must be at least 100ms',
               );
           return;
         }
@@ -195,6 +196,124 @@ List<BlockBluePrint> blockData = [
         return;
       }
       writeLog(valueToLog, ref);
+    },
+  ),
+  BlockBluePrint(
+    name: 'Activate Barometer',
+    fields: [],
+    children: [],
+    returnType: BlockTypes.none,
+    originalFunc: (WidgetRef ref, Block block) {
+      final barometerStream =
+          barometerEventStream(samplingPeriod: SensorInterval.fastestInterval)
+              .listen(
+        (event) {
+          ref.read(variablesProvider.notifier).setVariable(
+                "_pressure",
+                event.pressure,
+                BlockTypes.number,
+              );
+        },
+      );
+      ref.read(variablesProvider.notifier).setVariable(
+            "_barometerStream",
+            barometerStream,
+            BlockTypes.none,
+          );
+    },
+  ),
+  BlockBluePrint(
+    name: "Get Barometer",
+    returnType: BlockTypes.number,
+    fields: [],
+    children: [],
+    originalFunc: (WidgetRef ref, Block block) {
+      final value =
+          ref.read(variablesProvider.notifier).getVariable("_pressure");
+      if (value == null) {
+        print("Get Barometer: null");
+        return;
+      }
+      return value;
+    },
+  ),
+  BlockBluePrint(
+    name: 'Activate Accelerometer',
+    fields: [],
+    children: [],
+    returnType: BlockTypes.none,
+    originalFunc: (WidgetRef ref, Block block) {
+      final accelerometerStream = accelerometerEventStream(
+              samplingPeriod: SensorInterval.normalInterval)
+          .listen(
+        (event) {
+          ref.read(variablesProvider.notifier).setVariable(
+                "_accelerometerX",
+                event.x,
+                BlockTypes.number,
+              );
+          ref.read(variablesProvider.notifier).setVariable(
+                "_accelerometerY",
+                event.y,
+                BlockTypes.number,
+              );
+          ref.read(variablesProvider.notifier).setVariable(
+                "_accelerometerZ",
+                event.z,
+                BlockTypes.number,
+              );
+        },
+      );
+      ref.read(variablesProvider.notifier).setVariable(
+            "_accelerometerStream",
+            accelerometerStream,
+            BlockTypes.none,
+          );
+    },
+  ),
+  BlockBluePrint(
+    name: 'Get Accelerometer X',
+    fields: [],
+    children: [],
+    returnType: BlockTypes.number,
+    originalFunc: (WidgetRef ref, Block block) {
+      final value =
+          ref.read(variablesProvider.notifier).getVariable("_accelerometerX");
+      if (value == null) {
+        print("Get Accelerometer X: null");
+        return;
+      }
+      return value;
+    },
+  ),
+  BlockBluePrint(
+    name: 'Get Accelerometer Y',
+    fields: [],
+    children: [],
+    returnType: BlockTypes.number,
+    originalFunc: (WidgetRef ref, Block block) {
+      final value =
+          ref.read(variablesProvider.notifier).getVariable("_accelerometerY");
+      if (value == null) {
+        print("Get Accelerometer Y: null");
+        return;
+      }
+      return value;
+    },
+  ),
+  BlockBluePrint(
+    name: 'Get Accelerometer Z',
+    fields: [],
+    children: [],
+    returnType: BlockTypes.number,
+    originalFunc: (WidgetRef ref, Block block) {
+      final value =
+          ref.read(variablesProvider.notifier).getVariable("_accelerometerZ");
+      if (value == null) {
+        print("Get Accelerometer Z: null");
+        return;
+      }
+      return value;
     },
   ),
   BlockBluePrint(
@@ -339,6 +458,128 @@ List<BlockBluePrint> blockData = [
         return;
       }
       return value;
+    },
+  ),
+  BlockBluePrint(
+    name: 'Calculate Distance',
+    fields: [],
+    children: [
+      ValueInput(
+        label: 'Latitude 1',
+        block: null,
+        filter: [BlockTypes.number],
+      ),
+      ValueInput(
+        label: 'Longitude 1',
+        block: null,
+        filter: [BlockTypes.number],
+      ),
+      ValueInput(
+        label: 'Latitude 2',
+        block: null,
+        filter: [BlockTypes.number],
+      ),
+      ValueInput(
+        label: 'Longitude 2',
+        block: null,
+        filter: [BlockTypes.number],
+      ),
+    ],
+    returnType: BlockTypes.number,
+    originalFunc: (WidgetRef ref, Block block) {
+      final lat1 = block.children[0] as ValueInput;
+      final long1 = block.children[1] as ValueInput;
+      final lat2 = block.children[2] as ValueInput;
+      final long2 = block.children[3] as ValueInput;
+
+      if (lat1.block == null ||
+          long1.block == null ||
+          lat2.block == null ||
+          long2.block == null) {
+        ref.read(uiProvider.notifier).showMessage(
+              'Invalid input',
+            );
+        return null;
+      }
+
+      if (lat1.block!.execute(ref) == null ||
+          long1.block!.execute(ref) == null ||
+          lat2.block!.execute(ref) == null ||
+          long2.block!.execute(ref) == null) {
+        ref.read(uiProvider.notifier).showMessage(
+              'Invalid input',
+            );
+        return null;
+      }
+
+      final double distance = Geolocator.distanceBetween(
+        lat1.block!.execute(ref),
+        long1.block!.execute(ref),
+        lat2.block!.execute(ref),
+        long2.block!.execute(ref),
+      );
+      return distance;
+    },
+  ),
+  BlockBluePrint(
+    name: 'Calculate Bearing',
+    fields: [],
+    children: [
+      ValueInput(
+        label: 'Latitude 1',
+        block: null,
+        filter: [BlockTypes.number],
+      ),
+      ValueInput(
+        label: 'Longitude 1',
+        block: null,
+        filter: [BlockTypes.number],
+      ),
+      ValueInput(
+        label: 'Latitude 2',
+        block: null,
+        filter: [BlockTypes.number],
+      ),
+      ValueInput(
+        label: 'Longitude 2',
+        block: null,
+        filter: [BlockTypes.number],
+      ),
+    ],
+    returnType: BlockTypes.number,
+    originalFunc: (WidgetRef ref, Block block) {
+      final lat1 = block.children[0] as ValueInput;
+      final long1 = block.children[1] as ValueInput;
+      final lat2 = block.children[2] as ValueInput;
+      final long2 = block.children[3] as ValueInput;
+
+      if (lat1.block == null ||
+          long1.block == null ||
+          lat2.block == null ||
+          long2.block == null) {
+        ref.read(uiProvider.notifier).showMessage(
+              'Invalid input',
+            );
+        return null;
+      }
+
+      if (lat1.block!.execute(ref) == null ||
+          long1.block!.execute(ref) == null ||
+          lat2.block!.execute(ref) == null ||
+          long2.block!.execute(ref) == null) {
+        ref.read(uiProvider.notifier).showMessage(
+              'Invalid input',
+            );
+        return null;
+      }
+
+      final double bearing = Geolocator.bearingBetween(
+        lat1.block!.execute(ref),
+        long1.block!.execute(ref),
+        lat2.block!.execute(ref),
+        long2.block!.execute(ref),
+      );
+      return bearing;
     },
   ),
   BlockBluePrint(
@@ -520,7 +761,7 @@ List<BlockBluePrint> blockData = [
     },
   ),
   BlockBluePrint(
-    name: 'Int',
+    name: 'Number',
     fields: [
       Field(
         type: FieldTypes.number,
@@ -533,7 +774,7 @@ List<BlockBluePrint> blockData = [
     originalFunc: <num>(WidgetRef ref, Block block) {
       final value = block.fields[0].value;
       if (value is String) {
-        return int.parse(value);
+        return double.parse(value);
       } else if (value is num) {
         return value;
       } else {
